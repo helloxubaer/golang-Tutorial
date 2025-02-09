2191d283f0ad994c6f06acc777da7d1cc9cb1bd0
helloxubaer <xubaer.xeo@gmail.com>
2025-02-09 15:34:50 +0100

    mutex

--- 28-conventional-sync/main.go
+++ 28-conventional-sync/main.go
@@ -9,14 +9,18 @@ func do() int {
 	var value int
 	var w sync.WaitGroup
 
-	m := make(chan bool, 1)
+	// m := make(chan bool, 1) // counting semaphore, works as mutex
+	// better to use sync.Mutex
+	var m sync.Mutex
 
 	for i := 0; i < 1000; i++ {
 		w.Add(1)
 		go func() {
-			m <- true
+			// m <- true // write and block until read
+			m.Lock()
 			value++ // race condition
-			<-m
+			// <-m       // read
+			m.Unlock()
 			w.Done()
 		}()
 	}
